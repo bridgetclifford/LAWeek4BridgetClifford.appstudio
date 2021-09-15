a0e5384143fb8191eed08d27fc018577c741fcf6
bridgetclifford <90580691+bridgetclifford@users.noreply.github.com>
2021-09-15 14:35:02 -0500

    New code

---
+++ forms/pets/pets.js
@@ -0,0 +1,11 @@
+let petNames = ["Darcy", "Jack", "Jerry", "Marty","Vito", "Vinny", "Riley", "Saddie", "Marge", "Sam"]
+
+let withSal = petNames.push("Sal")
+
+console.log(`The third pet is named ${petNames[2]}.`)
+
+console.log(`The array has ${petNames.length} names in it.`)
+
+petNames.pop()
+
+console.log(petNames)
